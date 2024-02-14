3c60fff2392eb52c4f61970e8e9edf91e7b53a5a
PRANAY REDDY PALLE <pranayreddypalle@PRANAYs-MacBook-Pro.local>
2024-02-14 22:40:04 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,32 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import Navbar from "./Navbar";
+import MainContent from "./MainContent";
+import Footer from "./Footer.js";
+import AudioRecorder from "./AudioRecorder.js";
+import "./App.css"; // Assuming you have an App.css file for global styles
+import image from "./logoiiits.png"
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div>
+      <header>
+        <div style={{ display: "flex", alignItems: "center", textAlign: "center", paddingLeft: "100px" }}>
+          {/* Example logo image; replace 'logoSrc' with your actual logo path */}
+          <img
+            src={image}
+            alt="IIIT Sricity Logo"
+            style={{ marginRight: "8px", height: "50px" }}
+          />
+          <h1 style={{ textAlign: "center" }}>
+            IIIT Sricity's Spoken Keyword Spotting
+          </h1>
+        </div>
+        <Navbar />
       </header>
+      <MainContent />
+      <h2 style={{ textAlign: "center" }}>Record or Select an Audio File</h2>
+      <AudioRecorder />
+      <Footer />
     </div>
   );
 }
---
+++ src/Footer.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "./Footer.css";
+function Footer() {
+  return (
+    <footer className="container">
+      <small>
+        <a href="#">Privacy Policy</a> • <a href="#">Terms of Use</a>
+      </small>
+    </footer>
+  );
+}
+
+export default Footer;
